e0d7092af318ed29ba83be1a62c9444a57bcaef7
M Banks <montensia1@outlook.com>
2019-06-28 20:46:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
     
     get '/' do
-      erb :super_hero
+      erb :team
     end
     
     post '/teams' do
-      erb :team
+      erb :super_hero
     end
 
 
